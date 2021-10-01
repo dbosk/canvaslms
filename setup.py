6e34ae01746fc721e36ff73b6a2dd05de4eb6603
Daniel Bosk <dbosk@kth.se>
2021-10-01 19:07:53 +0200

    Updates version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "canvaslms",
-    version = "1.1",
+    version = "1.2",
     author = "Daniel Bosk",
     author_email = "dbosk@kth.se",
     description = "Command-line interface for Canvas LMS",
