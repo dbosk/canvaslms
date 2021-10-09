9b36d1dbc4901a508173955dc9681998b8f6662e
Daniel Bosk <dbosk@kth.se>
2021-10-09 09:13:05 +0200

    Bumps version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "canvaslms",
-    version = "1.2",
+    version = "1.3",
     author = "Daniel Bosk",
     author_email = "dbosk@kth.se",
     description = "Command-line interface for Canvas LMS",
